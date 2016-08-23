b02064033adfaaedc2c25cc0ca35740e0e91c028
Raykazi <Raykazi@users.noreply.github.com>
2016-08-23 17:23:05 -0400

    Commented

--- TrackerInterface/Helper.cs
+++ TrackerInterface/Helper.cs
@@ -6,13 +6,16 @@ using System.Threading.Tasks;
 
 namespace TrackerInterface
 {
+    //Functions that help the client and server run.
     public static class Helper
     {
+        //Convert time to Unix
         public static long ToUnixTime(this DateTime date)
         {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
             return Convert.ToInt64((date - epoch).TotalSeconds);
         }
+        //Convert to a human readable format
         public static DateTime FromUnixTime(long unixTime)
         {
             var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
