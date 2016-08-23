5af82b6248129503c398a498988406b163c12b8d
Raykazi <Raykazi@users.noreply.github.com>
2016-08-23 17:41:40 -0400

    Commented

--- TrackerService/Program.cs
+++ TrackerService/Program.cs
@@ -9,12 +9,14 @@ namespace TrackerService
 {
     class Program
     {
+        //Log events to the console window, with a timestamp for when they occured
         public static void ConsoleLog(string msg)
         {
             Console.WriteLine(string.Format("[{0}] {1}", DateTime.Now, msg));
         }
         static void Main(string[] args)
         {
+            //Host the server
             using (ServiceHost host = new ServiceHost(typeof(WCFTrackerService)))
             {
                 host.Open();
